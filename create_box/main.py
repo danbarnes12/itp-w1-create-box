580dc6bdf183deee055195682cd72425a58a90fc
Dan Barnes <danbarnes12@gmail.com>
2017-06-02 16:54:52 +0000

    First Commit

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,43 @@
 
 
 def create_box(height, width, character):
-    pass
+    if height < 1 or width < 1:
+        'Please enter height and width of at least 1'
+    height_counter = 1
+    width_counter = 1
+    box = character
+    while width_counter < width:
+        box += character
+        width_counter += 1
+    box += '\n' 
+    while height_counter < height:
+        for i in range(width_counter):
+            box += character
+        box += '\n'
+        height_counter += 1
+    return box
 
-
+def create_box_border(border_height,border_width,border_character):
+    if border_height < 1 or border_width < 1:
+        'Please enter border height and width of at least 1'
+    border_height_counter = 1
+    border_width_counter = 1
+    box_border = border_character
+    while border_width_counter < border_width:
+        box_border += border_character
+        border_width_counter += 1
+    box_border += '\n'
+    while border_height_counter < (border_height - 1):
+        box_border += border_character
+        for i in range(border_width_counter - 2):
+            box_border += ' '
+        box_border += border_character
+        box_border += '\n'
+        border_height_counter += 1
+    for i in range(border_width_counter):
+        box_border += border_character    
+    box_border += '\n'
+    return box_border
+    
 if __name__ == '__main__':
     create_box(3, 4, '*')
--- tests/test_main.py
+++ tests/test_main.py
@@ -18,6 +18,27 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+from create_box import create_box_border
+
+first_box_border_expected = """
+****
+*  *
+****
+""".lstrip()
+
+second_box_border_expected = """
+@@@@@
+@   @
+@   @
+@   @
+@@@@@
+""".lstrip()
+
+third_box__border_expected = """
+xxxxxxxxxxxxxxxxxxxxxxxx
+x                      x
+xxxxxxxxxxxxxxxxxxxxxxxx
+""".lstrip()
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -26,4 +47,15 @@ class TestCreateBox(unittest.TestCase):
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
 
-    # Add your own test using third_box_expected
+    def test_large_box(self):
+        self.assertEqual(create_box(3, 24, 'x'), third_box_expected)
+
+class TestCreateBoxBorder(unittest.TestCase):
+    def test_box_border(self):
+        self.assertEqual(create_box_border(3, 4, '*'), first_box_border_expected)
+
+    def test_medium_box_border(self):
+        self.assertEqual(create_box_border(5, 5, '@'), second_box_border_expected)
+
+    def test_large_box_border(self):
+        self.assertEqual(create_box_border(3, 24, 'x'), third_box__border_expected)
